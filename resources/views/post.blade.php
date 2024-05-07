72789997b56f34bed144d16ac93f1e1e47920fba
Aditya Rizki <adityarizki@192.168.1.12>
2024-05-07 08:41:56 +0700

    Konfigurasi Model

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <article class="py-8 max-w-screen-md border-b">
+        
+        <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
+  
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $post['author'] }}</a> | 5 Mei 2024
+        </div>
+        <p class="my-4 font-light">{{ $post['body'] }}
+        </p>
+        <a href="/posts/" class="font-medium text-blue-500 hover:underline">Kembali ke Blog &laquo;</a>
+    </article>
+    
+</x-layout>
