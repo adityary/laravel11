ee27fc5abe27c2009bfb6b2741f50a80a8fa5d6b
Aditya Rizki <adityarizki@192.168.1.5>
2024-05-16 11:37:50 +0700

    Eloquent ORM dan POST Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,47 +1,12 @@
-<?php   
+<?php
 
 namespace App\Models;
 
-use App\Models\Post;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return  [
-            [
-                'id' => '1',
-                'slug' => 'derita-sudah-naik-seleher',
-                'title' => 'Derita Sudah Naik Seleher',
-                'author' => 'Aditya Rizki',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, deleniti exercitationem ducimus corrupti dolores eius error nobis autem magnam fuga illo amet ipsa laborum officia unde at veritatis saepe! Voluptatum.'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'jogja-rindu-angkringan',
-                'title' => 'Jogja, Rindu, dan Angkringan',
-                'author' => 'Aditya Rizki',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis debitis delectus laudantium totam corporis? Praesentium eius nihil voluptates ab magnam quam dignissimos et quisquam dolore quasi? Fugiat rerum laboriosam laudantium!'
-            ]
-    
-            ];
-    }
-
-    public static function find($slug):array
-    {
-        // return Arr::first(static::all(), function($post) use ($slug) {
-        //    return $post['slug'] == $slug;
-        // });
-
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-        if (!$post) {
-            abort(404);
-        }
-        
-        return $post;
-    
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'slug', 'author', 'body'];
 }
-
-?>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
         <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
   
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 5 Mei 2024
+            <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="my-4 font-light">{{ $post['body'] }}
         </p>
